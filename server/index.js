ef7fea1a6ac60e9130af21a94e31134bd4b7bea3
Soumya Adhya <soumyaadhya007@gmail.com>
2023-06-27 23:45:48 +0530

    feat(index) :  Mount TodoRouter at the root URL

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 const app = express();
 const { connection } = require("./Config/db");
+const { TodoRouter } = require("./Routes/todo.routes");
 const PORT = process.env.PORT || 9000;
+app.use(cors()); // Enable Cross-Origin Resource Sharing (CORS)
+app.use(express.json()); // Parse JSON request bodies
+
+app.use("/", TodoRouter); // Mount TodoRouter at the root URL
 // Start the server
 app.listen(PORT, async () => {
   try {
